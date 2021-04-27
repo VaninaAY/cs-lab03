72390e34a9021ad631bfe3c53da4367ba8e193a2
VaninaAY <VaninaAY@mpei.ru>
2021-04-27 23:45:29 +0300

    маштабирование

--- main.cpp
+++ main.cpp
@@ -115,16 +115,46 @@ void show_histogram_svg(const vector<size_t>& bins)
     svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
 
     double top = 0;
-    for (size_t bin : bins)
-    {
-        const double bin_width = BLOCK_WIDTH * bin;
-        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
-        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT);
-        top += BIN_HEIGHT;
-    }
+
+    size_t max_count = 0;
+    for (size_t count : bins)
+        {
+        if (count > max_count)
+            max_count = count;
+        }
+
+        const bool scaling_needed = max_count > MAX_ASTERISK;
+
+        if (scaling_needed)
+        {
+            const double scaling_factor = (double)MAX_ASTERISK / max_count;
+
+            for (size_t bin : bins)
+            {
+                    auto  height = (size_t)(bin * scaling_factor);
+                    const double bin_width = BLOCK_WIDTH * height;
+                    svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+                    svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT);
+                    top += BIN_HEIGHT;
+            }
+
+        }
+
+        else
+        {
+            for (size_t bin : bins)
+            {
+                    const double bin_width = BLOCK_WIDTH * bin;
+                    svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+                    svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT);
+                    top += BIN_HEIGHT;
+            }
+
+        }
 
     svg_end();
 }
+
 
 int main()
 {
