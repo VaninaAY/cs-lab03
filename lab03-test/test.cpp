7c70f284546abb6974b7282a4dd914f2e66120cf
VaninaAY <VaninaAY@mpei.ru>
2021-04-29 17:30:32 +0300

    модульные тесты

---
+++ lab03-test/test.cpp
@@ -0,0 +1,56 @@
+#include "../histogram.h"
+#include <cassert>
+
+void test_positive()
+{
+    double min = 0;
+    double max = 0;
+    find_minmax({1, 2, 3}, min, max);
+    assert(min == 1);
+    assert(max == 3);
+}
+
+void test_negative()
+{
+    double min = 0;
+    double max = 0;
+    find_minmax({-1, -2, -3}, min, max);
+    assert(min == -3);
+    assert(max == -1);
+}
+
+void test_same_numbers()
+{
+    double min = 0;
+    double max = 0;
+    find_minmax({1, 1, 1}, min, max);
+    assert(min == 1);
+    assert(max == 1);
+}
+
+void test_single()
+{
+    double min=0;
+    double max=0;
+    find_minmax({1},min,max);
+    assert(min == 1);
+    assert(max == 1);
+}
+
+void test_empty ()
+{
+    double min=0;
+    double max=0;
+    find_minmax({},min,max);
+}
+
+
+
+int main()
+{
+    test_positive();
+    test_negative();
+    test_same_numbers();
+    test_single();
+    test_empty ();
+}
