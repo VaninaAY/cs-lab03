65c43f68798e3be82de56d97c42114e66189a39c
VaninaAY <VaninaAY@mpei.ru>
2021-04-29 16:58:41 +0300

    изменение(часть3)

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+#include "histogram.h"
+
 const size_t SCREEN_WIDTH = 80;
 const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
 
@@ -15,19 +17,6 @@ vector<double> input_numbers(size_t count)
     return result;
 }
 
-void find_minmax(const vector<double>& numbers, double& min, double& max)
-{
-    min = numbers[0];
-    max = numbers[0];
-    for (double number : numbers) {
-        if (number < min) {
-            min = number;
-        }
-        if (number > max) {
-            max = number;
-        }
-    }
-}
 
 vector<size_t> make_histogram(const vector<double>& numbers, size_t min, size_t max, size_t& bin_count)
 {
