2f4042a046b69bdb8c4c08d13ebc66b9be7925c9
VaninaAY <VaninaAY@mpei.ru>
2021-05-17 16:12:28 +0300

    защита

--- histogram.h
+++ histogram.h
@@ -2,8 +2,14 @@
 #define HISTOGRAM_H_INCLUDED
 
 #include <vector>
+#include <iostream>
 using namespace std;
 
+const size_t SCREEN_WIDTH = 80;
+const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
+
 void find_minmax(const vector<double>& numbers, double& min, double& max);
+vector<size_t> make_histogram(const vector<double>& numbers, size_t min, size_t max, size_t& bin_count);
+void show_histogram_text(const vector<size_t>& bins);
 
 #endif // HISTOGRAM_H_INCLUDED
