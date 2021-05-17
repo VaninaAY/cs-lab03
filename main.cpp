b25da6c06a358ec5da90c598891dbff8d3f995b1
VaninaAY <VaninaAY@mpei.ru>
2021-05-17 16:11:56 +0300

    защита

--- main.cpp
+++ main.cpp
@@ -7,8 +7,6 @@ using namespace std;
 #include "svg.h"
 #include "check.h"
 
-const size_t SCREEN_WIDTH = 80;
-const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
 
 vector<double> input_numbers(size_t count)
 {
@@ -18,57 +16,6 @@ vector<double> input_numbers(size_t count)
     }
     return result;
 }
-
-
-vector<size_t> make_histogram(const vector<double>& numbers, size_t min, size_t max, size_t& bin_count)
-{
-    vector<size_t> result(bin_count);
-    for (double number : numbers) {
-        size_t bin = (size_t)((number - min) / (max - min) * bin_count);
-        if (bin == bin_count) {
-            bin--;
-        }
-        result[bin]++;
-    }
-    return result;
-}
-
-void show_histogram_text(const vector<size_t>& bins)
-{
-    size_t max_count = 0;
-    for (size_t count : bins)
-        {
-        if (count > max_count)
-            max_count = count;
-        }
-
-    const bool scaling_needed = max_count > MAX_ASTERISK;
-
-    for (size_t bin : bins)
-    {
-        if (bin < 100)
-            cout << ' ';
-
-        if (bin < 10)
-            cout << ' ';
-
-        cout << bin << "|";
-
-        size_t height = bin;
-        if (scaling_needed)
-            {
-            const double scaling_factor = (double)MAX_ASTERISK / max_count;
-            height = (size_t)(bin * scaling_factor);
-            }
-
-        for (size_t i = 0; i < height; i++)
-            cout << '*';
-
-        cout << '\n';
-    }
-
-}
-
 
 
 int main()
