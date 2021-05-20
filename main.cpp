af6a7bebdc4f4018ec75548f6505c21a0240cbbc
VaninaAY <VaninaAY@mpei.ru>
2021-05-20 22:09:28 +0300

    3.2.9 Сохранение данных из сети в буфер

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <curl/curl.h>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
@@ -50,32 +52,56 @@ read_input(istream& in, bool promt) {
     return data;
 }
 
-int main(int argc, char* argv[])
-{
-    if (argc > 1)
+size_t
+write_data(void* items, size_t item_size, size_t item_count, void* ctx) {
+
+    size_t data_size = item_size * item_count;
+
+    stringstream* buffer = reinterpret_cast<stringstream*>(ctx);
+    buffer->write(reinterpret_cast<const char*>(items), data_size);
+
+    return data_size;
+}
+
+Input
+download(const string& address) {
+
+    stringstream buffer;
+    curl_global_init(CURL_GLOBAL_ALL);
+    CURL *curl = curl_easy_init();
+    if(curl)
     {
+        CURLcode res;
+        curl_easy_setopt(curl, CURLOPT_URL, address.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
+        res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
 
-        curl_global_init(CURL_GLOBAL_ALL);
-        CURL *curl = curl_easy_init();
-        if(curl)
+        if(res)
         {
-            CURLcode res;
-            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
-            res = curl_easy_perform(curl);
-            curl_easy_cleanup(curl);
-
-            if(res)
-            {
-                cout<<  curl_easy_strerror(res);
-                exit(1);
-            }
-
+            cout <<  curl_easy_strerror(res);
+            exit(1);
         }
-
-        return 0;
     }
 
-    const auto input = read_input(cin, true);
+    return read_input(buffer, false);
+}
+
+
+int main(int argc, char* argv[])
+{
+    Input input;
+    if (argc > 1)
+    {
+        input = download(argv[1]);
+    }
+
+    else
+    {
+         input = read_input(cin, true);
+    }
+
 
     // расчет количества чисел в столбцах гистограммы
     const auto bins = make_histogram(input);
